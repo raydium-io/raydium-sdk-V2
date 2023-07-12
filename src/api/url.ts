bb03fa51ec40f08f2d86413e3847f15708b4cd38
CruzShia <iamahan0720@gmail.com>
2023-07-12 11:53:32 +0800

    update api url constants

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,28 +1,34 @@
 export const API_URLS = {
-  BASE_HOST: "https://api.raydium.io/v2",
-  TOKEN: "/sdk/token/raydium.mainnet.json",
+  BASE_HOST: "https://api.raydium.io",
+  TOKEN_LIST: "/v3/mint/list",
+  TOKEN_INFO: "/v3/mint/item/{mint}",
+  TOKEN: "/v2/sdk/token/raydium.mainnet.json",
   COINGECKO: "https://api.coingecko.com/api/v3/simple/price",
-  LIQUIDITY: "/sdk/liquidity/mainnet.json",
+  LIQUIDITY: "/v2/sdk/liquidity/mainnet.json",
 
-  FARMS: "/sdk/farm-v2/mainnet.json",
+  FARMS: "/v2/sdk/farm-v2/mainnet.json",
   FARM_ARP: "/main/farm/info",
   FARM_ARP_LINE: "/main/farm-apr-tv",
 
-  AMM_V3: "/ammV3/ammPools",
-  AMM_V3_CONFIG: "/ammV3/ammConfigs",
-  AMM_V3_LINES: "/ammV3/positionLine",
+  AMM_V3: "/v2/ammV3/ammPools",
+  AMM_V3_CONFIG: "/v2/ammV3/ammConfigs",
+  AMM_V3_LINES: "/v2/ammV3/positionLine",
 
-  INFO: "/main/info",
-  VERSION: "/main/version",
+  INFO: "/v2/main/info",
+  VERSION: "/v2/main/version",
 
-  PAIRS: "/main/pairs",
-  PRICE: "/main/price",
-  RPCS: "/main/rpcs",
+  PAIRS: "/v2/main/pairs",
+  PRICE: "/v2/main/price",
+  RPCS: "/v2/main/rpcs",
 
-  CHAIN_TIME: "/main/chain/time",
+  CHAIN_TIME: "/v2/main/chain/time",
 
-  IDO_INFO: "/main/ido/pools",
-  IDO_PROJECT_INFO: "/main/ido/project/",
+  IDO_INFO: "/v2/main/ido/pools",
+  IDO_PROJECT_INFO: "/v2/main/ido/project/",
+};
+
+export const DEV_API_URLS = {
+  ...API_URLS,
 };
 
 export type API_URL_CONFIG = Partial<typeof API_URLS>;
