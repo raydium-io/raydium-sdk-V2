b82eabc98713d22d6700032507d622693bc377fd
Cruz Shia <iamahan0720@gmail.com>
2026-03-24 15:31:59 +0800

    add temp url

--- src/api/url.ts
+++ src/api/url.ts
@@ -4,6 +4,7 @@ export const API_URLS = {
   SERVICE_BASE_HOST: "https://service.raydium.io",
   MONITOR_BASE_HOST: "https://monitor.raydium.io",
   SERVICE_1_BASE_HOST: "https://service-v1.raydium.io",
+  TEMP_HOST: "https://temp-api-v1.raydium.io",
 
   SEND_TRANSACTION: "/send-transaction",
   FARM_ARP: "/main/farm/info",
@@ -87,6 +88,7 @@ export const DEV_API_URLS = {
   OWNER_BASE_HOST: "https://owner-v1-devnet.raydium.io",
   SWAP_HOST: "https://transaction-v1-devnet.raydium.io",
   CPMM_LOCK: "https://dynamic-ipfs-devnet.raydium.io/lock/cpmm/position",
+  TEMP_HOST: "https://temp-api-v1-devnet.raydium.io",
 
   LAUNCH_AUTH_HOST: "https://launch-auth-v1-devnet.raydium.io",
   LAUNCH_COMMENT_HOST: "https://launch-forum-v1-devnet.raydium.io",
